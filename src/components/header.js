f027c4d4739ead3ebdb60d028522e600136c4a8d
Lorenze Jay <Lorenzejay@gmail.com>
2020-12-18 11:10:24 -0800

    responsive header

--- src/components/header.js
+++ src/components/header.js
@@ -1,42 +1,95 @@
 import { Link } from "gatsby"
-import PropTypes from "prop-types"
-import React from "react"
+import React, { useState } from "react"
+import styled from "styled-components"
+import { graphql, useStaticQuery } from "gatsby"
+import "../styles/header.scss"
+import "../styles/responsive.scss"
+const Navbar = styled.header`
+  z-index: 100;
+  height: 10vh;
+  display: flex;
+  position: relative;
+  background: white;
+  padding: 0 5%;
+  align-items: center;
+  width: 100%;
+  box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
+`
+const NavLinks = styled.div`
+  margin-left: auto;
+  display: flex;
+`
+const NavLink = styled.div`
+  a {
+    color: #333;
+    padding: 0 16px;
+    text-decoration: none;
+    font-size: 16px;
+    font-weight: bold;
+    text-transform: uppercase;
+    &:hover {
+      text-decoration: underline;
+    }
+  }
+`
+const Branding = styled.div`
+  margin: auto 0;
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
+  a {
+    color: #333;
+    padding: 0 16px;
+    text-decoration: none;
+    font-size: 24px;
+    font-weight: bold;
+    text-transform: uppercase;
+  }
+`
+const Header = () => {
+  const [burgerActive, setBurgerActive] = useState(false)
+  const data = useStaticQuery(graphql`
+    query NavQuery {
+      allPrismicNavigation {
+        edges {
+          node {
+            data {
+              branding
+              navigation_links {
+                label
+                link {
+                  uid
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  `)
+  const navigationLinks =
+    data.allPrismicNavigation.edges[0].node.data.navigation_links
+  return (
+    <Navbar className="navbar">
+      <Branding>
+        <Link to="/">
+          {data.allPrismicNavigation.edges[0].node.data.branding}
         </Link>
-      </h1>
-    </div>
-  </header>
-)
-
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
-
-Header.defaultProps = {
-  siteTitle: ``,
+      </Branding>
+      <NavLinks className={`nav-links ${burgerActive ? "nav-active" : ""}`}>
+        {navigationLinks.map(link => {
+          return (
+            <NavLink key={link.link.uid}>
+              <Link to={`/${link.link.uid}`}>{link.label}</Link>
+            </NavLink>
+          )
+        })}
+      </NavLinks>
+      <div className="burger" onClick={() => setBurgerActive(!burgerActive)}>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </Navbar>
+  )
 }
 
 export default Header
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,114 +1,18 @@
 import React, { useState, useEffect } from "react"
 import PropTypes from "prop-types"
-import { useStaticQuery, graphql, Link } from "gatsby"
 import styled from "styled-components"
 import "./layout.css"
 import Footer from "./footer"
-
+import Header from "./header"
+import "../styles/responsive.scss"
 const MainWrapper = styled.main`
   margin: 0 auto;
 `
-const Header = styled.header`
-  z-index: 100;
-  height: 10vh;
-  display: flex;
-  position: relative;
-  background: white;
-  padding: 0 5%;
-  align-items: center;
-  width: 100%;
-  box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
-`
-const NavLinks = styled.div`
-  margin-left: auto;
-  display: flex;
-`
-const NavLink = styled.div`
-  a {
-    color: #333;
-    padding: 0 16px;
-    text-decoration: none;
-    font-size: 16px;
-    font-weight: bold;
-
-    &:hover {
-      text-decoration: underline;
-    }
-  }
-`
-const Branding = styled.div`
-  margin: auto 0;
-
-  a {
-    color: #333;
-    padding: 0 16px;
-    text-decoration: none;
-    font-size: 24px;
-    font-weight: bold;
-    text-transform: uppercase;
-  }
-`
 
 const Layout = ({ children }) => {
-  const [scrolling, setScrolling] = useState(false)
-  const [scrollPosition, setScrollPosition] = useState(0)
-  const data = useStaticQuery(graphql`
-    query NavQuery {
-      allPrismicNavigation {
-        edges {
-          node {
-            data {
-              branding
-              navigation_links {
-                label
-                link {
-                  uid
-                }
-              }
-            }
-          }
-        }
-      }
-    }
-  `)
-
-  useEffect(() => {
-    window.addEventListener("scroll", e => {
-      if (window.scrollY !== 0) {
-        setScrolling(true)
-      } else {
-        setScrolling(false)
-      }
-    })
-  })
-  // useEffect(() => {
-  //   console.log(window.scrollY)
-  //   if (window.scrollY !== 0) {
-  //     setScrollPosition(window.scrollY)
-  //   }
-  // })
-
-  const navigationLinks =
-    data.allPrismicNavigation.edges[0].node.data.navigation_links
-
   return (
     <>
-      <Header scrolling={scrolling}>
-        <Branding>
-          <Link to="/">
-            {data.allPrismicNavigation.edges[0].node.data.branding}
-          </Link>
-        </Branding>
-        <NavLinks>
-          {navigationLinks.map(link => {
-            return (
-              <NavLink key={link.link.uid}>
-                <Link to={`/${link.link.uid}`}>{link.label}</Link>
-              </NavLink>
-            )
-          })}
-        </NavLinks>
-      </Header>
+      <Header />
       <MainWrapper>{children}</MainWrapper>
       <Footer />
     </>
