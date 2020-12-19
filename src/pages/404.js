bb85387ef72417f5562bc4814103905486c13109
Lorenze Jay <Lorenzejay@gmail.com>
2020-12-18 18:45:59 -0800

    created 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,36 @@
 import React from "react"
-
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import styled from "styled-components"
+import { Link } from "gatsby"
+
+const _404Page = styled.section`
+  height: 90vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  h1 {
+    font-size: 4rem;
+  }
+  a {
+    color: #333;
+    text-decoration: none;
+    font-weight: bold;
+    text-transform: uppercase;
+  }
+`
 
 const NotFoundPage = () => (
   <Layout>
-    <SEO title="404: Not found" />
-    <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <SEO title="Uplift | 404" />
+    <_404Page>
+      <h1>404</h1>
+      <p>This page does not exist :(</p>
+      <p>
+        But let's bring you a page that does -<Link to="/"> Home</Link>
+      </p>
+    </_404Page>
   </Layout>
 )
 
